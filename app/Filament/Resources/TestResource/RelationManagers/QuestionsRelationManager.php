71d749fc2f4de2a4d9b3bb939f4dfe84325ac2df
Dauren <Dauren.Kozhakhmetov@alsi.com>
2026-06-12 09:22:00 +0500

    add test service

--- app/Filament/Resources/TestResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/TestResource/RelationManagers/QuestionsRelationManager.php
@@ -24,7 +24,7 @@ class QuestionsRelationManager extends RelationManager
                     ->schema([
                         Forms\Components\Select::make('question_type_id')
                             ->label('Тип вопроса')
-                            ->relationship('questionType', 'name')
+                            ->relationship('type', 'name')
                             ->preload()
                             ->required(),
 
