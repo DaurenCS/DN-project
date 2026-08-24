ff302dfe54fd0ca6b5930451415383060be7658a
daurenkozahmetov <dau.04.04@mail.ru>
2026-08-24 21:06:31 +0500

    new commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,15 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();
         $middleware->append(\App\Http\Middleware\SetLocale::class);
-        $middleware->append(\App\Http\Middleware\EnsureUserHasRole::class);
-        $middleware->trustProxies(at: '*');
-
+        $middleware->alias([
+            'role' => \App\Http\Middleware\EnsureUserHasRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (\Throwable $e, $request) {
